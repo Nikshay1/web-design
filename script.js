89d4423a4868e2f835ba507bcf642494b8aeec72
Nikshay1 <142086467+Nikshay1@users.noreply.github.com>
2024-01-22 23:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+body {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
 
+.header {
+    background-color: antiquewhite;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.header .left {
+    width: 96%;
+    height: 80px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;    
+}
+
+.header img {
+    width: 200px;   
+    height: auto;
+    margin-right: auto;
+}
